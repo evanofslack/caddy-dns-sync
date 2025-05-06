8c0308a4a8f7ef3753aec195008b67109e6f3241
Evan Slack <evan.slack@outlook.com>
2025-05-06 01:24:39 -0400

    config all from env

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,7 +1,12 @@
 package config
 
 import (
+	"errors"
+	"io/fs"
+	"log/slog"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 
 	"gopkg.in/yaml.v3"
@@ -9,10 +14,10 @@ import (
 
 const (
 	defaultSyncInterval = time.Minute
-	defaultStatePath    = "caddy-sync-dns.db"
+	defaultStatePath    = "caddydnssync.db"
 	defaultOwner        = "default"
-	defaultLogLevel = "info"
-	defaultLogEnv = "prod"
+	defaultLogLevel     = "info"
+	defaultLogEnv       = "prod"
 )
 
 type Config struct {
@@ -31,7 +36,7 @@ type Caddy struct {
 type DNS struct {
 	Provider string   `yaml:"provider"`
 	Zones    []string `yaml:"zones"`
-	Token    string   `yaml:"token"` // Value will be overridden by environment variable
+	Token    string   `yaml:"token"`
 	TTL      int      `yaml:"ttl"`
 }
 
@@ -47,16 +52,27 @@ type Reconcile struct {
 }
 
 func Load(path string) (*Config, error) {
-	f, err := os.Open(path)
-	if err != nil {
-		return nil, err
+	configFile := true
+	_, err := os.Stat(path)
+	if errors.Is(err, fs.ErrNotExist) {
+		slog.Default().Warn("fail find config file, proceeding", "path", path)
+		configFile = false
 	}
-	defer f.Close()
 
 	var cfg Config
-	decoder := yaml.NewDecoder(f)
-	if err := decoder.Decode(&cfg); err != nil {
-		return nil, err
+	if configFile {
+		f, err := os.Open(path)
+		if err != nil {
+			return nil, err
+		}
+
+		decoder := yaml.NewDecoder(f)
+		if err := decoder.Decode(&cfg); err != nil {
+			return nil, err
+		}
+		if err := f.Close(); err != nil {
+			slog.Default().Warn("fail close config file", "path", path, "error", err)
+		}
 	}
 
 	if cfg.SyncInterval == 0 {
@@ -79,10 +95,59 @@ func Load(path string) (*Config, error) {
 		cfg.Log.Env = "prod"
 	}
 
-
-	// Override token from environment if set
-	if token := os.Getenv("CLOUDFLARE_API_TOKEN"); token != "" {
+	// Override from environment if set
+	if token := os.Getenv("CADDY_DNS_SYNC_CLOUDFLARE_TOKEN"); token != "" {
 		cfg.DNS.Token = token
+	}
+	if syncInterval := os.Getenv("CADDY_DNS_SYNC_INTERVAL"); syncInterval != "" {
+		if interval, err := time.ParseDuration(syncInterval); err != nil {
+			cfg.SyncInterval = interval
+		} else {
+			slog.Default().Warn("fail parse sync interval to duration from string", "interval", interval, "error", err)
+		}
+	}
+	if statePath := os.Getenv("CADDY_DNS_SYNC_STATE_PATH"); statePath != "" {
+		cfg.StatePath = statePath
+	}
+	if caddyUrl := os.Getenv("CADDY_DNS_SYNC_CADDY_URL"); caddyUrl != "" {
+		cfg.Caddy.AdminURL = caddyUrl
+	}
+	if dnsProvider := os.Getenv("CADDY_DNS_SYNC_PROVIDER"); dnsProvider != "" {
+		cfg.DNS.Provider = dnsProvider
+	}
+	if dnsZones := os.Getenv("CADDY_DNS_SYNC_ZONES"); dnsZones != "" {
+		zones := strings.Split(dnsZones, ",")
+		cfg.DNS.Zones = zones
+	}
+	if dnsTtl := os.Getenv("CADDY_DNS_SYNC_TTL"); dnsTtl != "" {
+		if ttl, err := strconv.Atoi(dnsTtl); err != nil {
+			cfg.DNS.TTL = ttl
+		} else {
+			slog.Default().Warn("fail parse ttl to int from string", "ttl", dnsTtl, "error", err)
+		}
+	}
+	if dryRun := os.Getenv("CADDY_DNS_SYNC_DRYRUN"); dryRun != "" {
+		switch strings.ToLower(dryRun) {
+		case "true":
+			cfg.Reconcile.DryRun = true
+		case "false":
+			cfg.Reconcile.DryRun = false
+		default:
+			slog.Default().Warn("fail parse dryrun to bool from string", "dryrun", dryRun)
+		}
+	}
+	if owner := os.Getenv("CADDY_DNS_SYNC_OWNER"); owner != "" {
+		cfg.Reconcile.Owner = owner
+	}
+	if protectedRecords := os.Getenv("CADDY_DNS_SYNC_PROTECTED_RECORDS"); protectedRecords != "" {
+		records := strings.Split(protectedRecords, ",")
+		cfg.Reconcile.ProtectedRecords = records
+	}
+	if loglevel := os.Getenv("CADDY_DNS_SYNC_LOG_LEVEL"); loglevel != "" {
+		cfg.Log.Level = loglevel
+	}
+	if logenv := os.Getenv("CADDY_DNS_SYNC_LOG_ENV"); logenv != "" {
+		cfg.Log.Env = logenv
 	}
 	return &cfg, nil
 }
